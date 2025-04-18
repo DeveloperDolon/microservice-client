6ac2bcae7bd3d80eec3d25a7e2a64330fc1c88a6
DeveloperDolon <dolonr718@gmail.com>
2025-04-18 23:06:56 +0600

    docker complete setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
   <div>
-    Hello world form bangladesh.
+    Hello world form bangladesh. kamon acho dado.
   </div>
   );
 }
