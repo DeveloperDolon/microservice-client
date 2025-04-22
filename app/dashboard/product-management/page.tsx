32138c8657173ca0f2d36907d0f452114f98785e
DeveloperDolon <dolonr718@gmail.com>
2025-04-22 21:14:10 +0600

    product management page title and button added

--- app/dashboard/product-management/page.tsx
+++ app/dashboard/product-management/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Button } from 'antd';
+import { PlusCircleFilled } from '@ant-design/icons';
 
 const page = () => {
   return (
     <div>
-      Hello world from product management page.
+      <div className='flex justify-between items-center mb-4'>
+        <h1 className="text-2xl font-bold">Product Management</h1>
+        <Button type="primary" icon={<PlusCircleFilled />} size={'large'}>
+            Add Product
+          </Button>
+      </div>
     </div>
   );
 };
