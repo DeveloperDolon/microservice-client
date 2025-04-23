107f43f8d3d72dab4858fe4e45d6e20691cd3943
DeveloperDolon <dolonr718@gmail.com>
2025-04-23 13:31:06 +0600

    product type added

--- app/_validations/product_validation.ts
+++ app/_validations/product_validation.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const productValidation = z.object({
+export const product_validation = z.object({
     name: z.string().min(1, "Product name is required"),
     images: z.array(z.instanceof(File)).min(1, "At least one image is required"),
     discount: z.number().optional(),
@@ -8,7 +8,7 @@ export const productValidation = z.object({
     description: z.string().min(50, "Description must be at least 50 characters"),
     discount_type: z.enum(["percentage", "amount"]).default('percentage'),
     ingredients: z.array(z.string()).optional(),
-    shipping_cost: z.number().optional(),
+    shipping_cost: z.number(),
     benefit: z.string().optional(),
     seller_id: z.string().min(1, "Seller is required"),
     likes: z.number().optional(),
