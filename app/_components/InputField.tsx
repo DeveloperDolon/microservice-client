f62467c5ff6348f3c2ec394a86f214a04c56b851
DeveloperDolon <dolonr718@gmail.com>
2025-04-23 13:17:14 +0600

    reasuable component for input field  logic changed

--- app/_components/InputField.tsx
+++ app/_components/InputField.tsx
@@ -1,16 +1,9 @@
-import { Input } from "antd";
+import { Input, Select } from "antd";
+import Checkbox from "antd/es/checkbox/Checkbox";
+import TextArea from "antd/es/input/TextArea";
 import { RegisterOptions, useFormContext } from "react-hook-form";
 
 interface InputFieldProps {
-  //   register: (
-  //     name: string,
-  //     options?: RegisterOptions
-  //   ) => {
-  //     ref: unknown;
-  //     name: string;
-  //     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  //     onBlur: () => void;
-  //   };
   name: string;
   label: string;
   placeholder: string;
@@ -34,18 +27,69 @@ const InputField = ({
   const error: string = (errors[name]?.message as string) || "";
   return (
     <div className={`flex flex-col ${className}`}>
-      <label htmlFor={name} className="text-sm">
-        {label}
-      </label>
-      <Input
-        className="md:px-4 px-3 mt-1"
-        size="large"
-        variant="filled"
-        type={type}
-        placeholder={placeholder}
-        {...register(name, options)}
-      />
-      {error && <p className="">{error}</p>}
+      {type === "textarea" ? (
+        <>
+          <label htmlFor={name} className="text-sm">
+            {label}
+          </label>
+          <TextArea
+            className="mt-1"
+            size="large"
+            variant="filled"
+            placeholder={placeholder}
+            {...register(name, options)}
+          />
+          {error && <p className="">{error}</p>}
+        </>
+      ) : type === "select" ? (
+        <>
+          <label htmlFor={name} className="text-sm">
+            {label}
+          </label>
+          <Select
+            className="mt-1"
+            showSearch
+            placeholder={placeholder}
+            {...register(name, options)}
+            filterOption={(input, option) =>
+              (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+            }
+            options={[
+              { value: "1", label: "Jack" },
+              { value: "2", label: "Lucy" },
+              { value: "3", label: "Tom" },
+            ]}
+          />
+          {error && <p className="">{error}</p>}
+        </>
+      ) : type === "checkbox" ? (
+        <>
+          <label htmlFor={name} className="text-sm">
+            {label}
+          </label>
+          <Checkbox className="mt-1" type={type} {...register(name, options)}>
+            Checkbox
+          </Checkbox>
+          {error && <p className="">{error}</p>}
+        </>
+      ) : type === "text" ? (
+        <>
+          <label htmlFor={name} className="text-sm">
+            {label}
+          </label>
+          <Input
+            className="mt-1"
+            size="large"
+            variant="filled"
+            type={type}
+            placeholder={placeholder}
+            {...register(name, options)}
+          />
+          {error && <p className="">{error}</p>}
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
