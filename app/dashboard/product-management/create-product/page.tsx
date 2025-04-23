e0595be8bef9ec1d4165beef8f25bd8316f4d8d0
DeveloperDolon <dolonr718@gmail.com>
2025-04-23 11:26:01 +0600

    create product page added and react hook form with zod validation added

--- app/dashboard/product-management/create-product/page.tsx
+++ app/dashboard/product-management/create-product/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      This is the create product page
+      <h1 className="md:text-2xl text-lg font-bold text-center">Create Product</h1>
     </div>
   );
 };
--- app/dashboard/product-management/page.tsx
+++ app/dashboard/product-management/page.tsx
@@ -63,7 +63,7 @@ const page = () => {
   return (
     <div>
       <div className="flex justify-between items-center mb-4">
-        <h1 className="text-2xl font-bold">Product Management</h1>
+        <h1 className="md:text-2xl text-lg font-bold">Product Management</h1>
 
         <Search
           className="md:w-[500px] w-[300px]"
