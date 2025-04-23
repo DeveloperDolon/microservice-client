774c25515bb2658ae5aa41f3ae7da789df9c53a8
DeveloperDolon <dolonr718@gmail.com>
2025-04-23 12:15:18 +0600

    reasuable form input component added

---
+++ app/_components/InputField.tsx
@@ -0,0 +1,42 @@
+import { RegisterOptions, useFormContext } from "react-hook-form";
+
+interface InputFieldProps {
+//   register: (
+//     name: string,
+//     options?: RegisterOptions
+//   ) => {
+//     ref: unknown;
+//     name: string;
+//     onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+//     onBlur: () => void;
+//   };
+  name: string;
+  label: string;
+  placeholder: string;
+  type: string;
+  options?: RegisterOptions;
+  className?: string;
+}
+
+const InputField = ({
+  name,
+  label,
+  placeholder,
+  type,
+  options,
+  className,
+}: InputFieldProps) => {
+    const { register, formState: {errors},  } = useFormContext();
+    const error: string = errors[name]?.message as string || '';
+  return (
+    <div className={`flex flex-col ${className}`}>
+      <label htmlFor={name} className="text-sm font-semibold">
+        {label}
+      </label>
+      <input type={type} placeholder={placeholder} {...register(name, options)} />
+      {error && <p className="">{error}</p>}
+    </div>
+  );
+};
+
+export default InputField;
--- app/dashboard/product-management/create-product/page.tsx
+++ app/dashboard/product-management/create-product/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import InputField from "@/app/_components/InputField";
 import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
@@ -10,7 +11,21 @@ const Page = () => {
         Create Product
       </h1>
 
-      <FormProvider {...methods}>hello</FormProvider>
+      <FormProvider {...methods}>
+        <InputField
+          name="name"
+          label="Product Name"
+          placeholder="Enter product name"
+          type="text"
+          options={{
+            required: "Product name is required",
+            minLength: {
+              value: 3,
+              message: "Product name must be at least 3 characters",
+            },
+          }}
+        />
+      </FormProvider>
     </div>
   );
 };
