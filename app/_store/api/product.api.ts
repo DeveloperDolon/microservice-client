a75ad37dbce9bf1ddb593a897ed2923a87582776
DeveloperDolon <dolonr718@gmail.com>
2025-05-03 21:10:16 +0600

    product delete api implemented

--- app/_store/api/product.api.ts
+++ app/_store/api/product.api.ts
@@ -10,22 +10,28 @@ const productApi = baseApi.injectEndpoints({
       }),
     }),
     productList: builder.query({
-        query: (params) => ({
-            url: "/product/list",
-            params: params,
-            method: "GET"
-        })
+      query: (params) => ({
+        url: "/product/list",
+        params: params,
+        method: "GET",
+      }),
     }),
     productShow: builder.query({
-        query: (id) => (`/product/show/${id}`)
+      query: (id) => `/product/show/${id}`,
     }),
     productUpdate: builder.mutation({
-        query: ({id, payload}) => ({
-            url: `/product/update/${id}`,
-            body: payload,
-            method: "PUT"
-        })
-    })
+      query: ({ id, payload }) => ({
+        url: `/product/update/${id}`,
+        body: payload,
+        method: "PUT",
+      }),
+    }),
+    productDelete: builder.mutation({
+      query: (id) => ({
+        url: `/product/delete/${id}`,
+        method: "DELETE",
+      }),
+    }),
   }),
 });
 
