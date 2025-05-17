a2bcdc450dc41d6034641881ebc0a5972f3d1bf2
DeveloperDolon <dolonr718@gmail.com>
2025-05-17 12:22:21 +0600

    user create page added

--- app/dashboard/user-management/create-user/page.tsx
+++ app/dashboard/user-management/create-user/page.tsx
@@ -1,14 +1,36 @@
+"use client";
+
 import user_create_validation from "@/app/_validations/user_validation";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserType } from "@/app/_types/user_types";
+import { Button, message } from "antd";
+import Link from "next/link";
 
 const Page = () => {
   const methods = useForm<UserType<File>>({
     resolver: zodResolver(user_create_validation),
   });
+  const [messageApi, contextHolder] = message.useMessage();
 
-  return <div>Hello world from create user.</div>;
+  return (
+    <div>
+      <Link href={"/dashboard/user-management"}>
+        <Button>Back</Button>
+      </Link>
+      {contextHolder}
+      <h1 className="md:text-2xl text-lg font-bold text-center">
+        Create User
+      </h1>
+
+      <FormProvider {...methods}>
+        <form
+          //   onSubmit={methods.handleSubmit(onSubmit)}
+          className="grid gap-4 md:grid-cols-2 grid-cols-1 mt-6"
+        ></form>
+      </FormProvider>
+    </div>
+  );
 };
 
 export default Page;
